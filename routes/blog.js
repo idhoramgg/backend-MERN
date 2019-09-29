53b8db4f73d9fcf24102ea8dd0af28324a06d37c
idhoramgg <ridhoabdulmajid@gmail.com>
2019-09-30 04:01:33 +0700

    reroute

--- routes/blog.js
+++ routes/blog.js
@@ -1,23 +1,27 @@
 const express = require('express')
+const passport = require('passport')
 // const auth = require('../helpers/jwt-token')
 const router = express.Router()
+const Blog = require('../controllers/blog')
+const requireAuth = passport.authenticate('jwt', { session: false });
+const requireSignin = passport.authenticate('local', { session: false });
 
-const {
-  fetchPosts,
-  createPost,
-  fetchPost,
-  allowUpdateOrDelete,
-  updatePost,
-  deletePost,
-  fetchPostByAuthorId
-} = require('../controllers/blog')
+// const {
+//   fetchPosts,
+//   createPost,
+//   fetchPost,
+//   allowUpdateOrDelete,
+//   updatePost,
+//   deletePost,
+//   fetchPostByAuthorId
+// } = require('../controllers/blog')
 
-router.get('/posts', fetchPosts);
-router.get('/posts/:id', fetchPost);
-router.get('allow_edit_or_delete/:id', allowUpdateOrDelete);
-router.get('/my_posts', fetchPostByAuthorId);
-router.post('/posts', createPost);
-router.put('/posts/:id', updatePost);
-router.delete('/posts/:id', deletePost);
+router.get('/posts', requireAuth, Blog.fetchPosts);
+router.get('/posts/:id', requireAuth, Blog.fetchPost);
+router.get('allow_edit_or_delete/:id', requireAuth, Blog.allowUpdateOrDelete);
+router.get('/my_posts', requireAuth, Blog.fetchPostByAuthorId);
+router.post('/posts', requireAuth, Blog.createPost);
+router.put('/posts/:id', requireAuth, Blog.updatePost);
+router.delete('/posts/:id', requireAuth, Blog.deletePost);
 
 module.exports = router;
